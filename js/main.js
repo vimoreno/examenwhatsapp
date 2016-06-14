55b76e0c3e6129c3891ff4f9e7edf55c43cd9193
Vianey Moreno <morenogomezvianey@gmail.com>
2016-06-14 20:26:29 +0000

    ulitmo

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$( "button" ).click(function() {
+  $( "a" ).toggle( "slow" );
+});
+
+
+$(document).ready(function() {
+   
+    $('#raymi').append('<div>Haz visto el corte de aldo?</div>');
+    
+});
+
+
+$(document).ready(function() {
+   
+    $('#maria').append('<div>Haz visto el corte de aldo?</div>');
+    
+});
+
+$(document).ready(function() {
+   
+    $('#mariana').append('<div>Haz visto el corte de aldo?</div>');
+    
+});
+
+$(document).ready(function() {
+   
+    $('#rodulfo').append('<div>Haz visto el corte de aldo?</div>');
+    
+});
+
+
+
+$(document).ready(function() {
+   
+    $('#aldo').append('<div>Haz visto el corte de aldo?</div>');
+    
+});
+
+
+$(document).ready(function() {
+   
+    $('#andrea').append('<div>Haz visto el corte de aldo?</div>');
+    
+});
+
+
+//Deshice como mil veces el codigo//
+
+
